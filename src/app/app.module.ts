929e8c88fd73d27be112bbdc17fc0a9d16e3b9b3
azharsquared <azharsquared@gmail.com>
2021-02-24 13:59:41 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
+import { TodoListBodyComponent } from './todo-list-body/todo-list-body.component';
+import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListHeaderComponent,
+    TodoListBodyComponent,
+    TodoListFooterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo-list-body/todo-list-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-body',
+  templateUrl: './todo-list-body.component.html',
+  styleUrls: ['./todo-list-body.component.css']
+})
+export class TodoListBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
